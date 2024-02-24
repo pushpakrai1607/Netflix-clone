0b19940e0df4955fc1218f30757d76a5050cfe9c
Pushpak rai <pushpakrai1607@gmail.com>
2024-02-24 23:52:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import Body from "./components/bodyComponent/Body";
+import "./index.css";
+import appStore from "../src/utils/store/userStore";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+{/* //57e94b014e156c5f726cfc9197a74e9e */}
+      
+      <Provider store={appStore}>
+        <Body />
+      </Provider>
     </div>
   );
 }
